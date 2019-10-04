29f468d54742ea986a054e2ac13a8de8856f0bd1
Harace Moon <mym0846@gmail.com>
2019-10-04 12:35:03 +0900

    Change filterL logic
    - Add promise process logic

--- lazy/filterL.js
+++ lazy/filterL.js
@@ -1,6 +1,13 @@
 import curry from '../base/curry';
 import goFirst from '../base/goFirst';
+import nop from '../base/nop';
+import isPromise from '../base/isPromise';
+import toIter from '../base/toIter';
 
 export default curry(function *filterL(f, iter) {
-    for (const a of iter) if(goFirst(a, f)) yield a;
+  for (const a of toIter(iter)) {
+    const b = goFirst(a, f);
+    if (isPromise(b)) yield b.then(b => b ? a : Promise.reject(nop));
+    else if (b) yield a;
+  }
 });
