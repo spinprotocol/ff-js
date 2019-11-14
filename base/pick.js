0e4fa2f6d3da23626558bb9813fde37298436031
Harace Moon <mym0846@gmail.com>
2019-11-14 15:26:25 +0900

    Bug fix in pick.jsd

--- base/pick.js
+++ base/pick.js
@@ -1,6 +1,7 @@
 import entriesL from "../lazy/entriesL";
 import filterL from "../lazy/filterL";
 import object from "./object"
+import go from "./go";
 import curry from "./curry";
 
 export default curry(function pick(ks, obj) {
